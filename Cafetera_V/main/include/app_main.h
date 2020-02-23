ce78a9179087065b7e2d9fc1fac2aac2575abc4e
Many3Stars <61276772+Many3Stars@users.noreply.github.com>
2020-02-23 18:27:37 +0100

    Add files via upload

---
+++ Cafetera_V/main/include/app_main.h
@@ -0,0 +1,33 @@
+#include "app_camera.h"
+#include "app_httpserver.h"
+#include "app_wifi.h"
+#include "app_speech_srcif.h"
+
+#define VERSION "0.9.0"
+
+#define GPIO_LED_RED    21
+#define GPIO_LED_WHITE  22
+#define GPIO_BUTTON     15
+
+#define	DATAOUT		2
+#define	DATAIN		12
+#define	CLK			0
+#define	CS			19
+
+typedef enum
+{
+    WAIT_FOR_WAKEUP,
+    WAIT_FOR_CONNECT,
+    START_DETECT,
+    START_RECOGNITION,
+    START_ENROLL,
+    START_DELETE,
+
+} en_fsm_state;
+
+extern en_fsm_state g_state;
+extern int g_is_enrolling;
+extern int g_is_deleting;
+//////
+extern int g_is_eleccion;
+//////
